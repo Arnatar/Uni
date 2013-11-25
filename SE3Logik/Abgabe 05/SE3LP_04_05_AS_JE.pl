02df0cbd160e85a3d95156307ef2f1e8ca3c8472
Jan <jan@vanesdonk.de>
2013-11-25 16:32:24 +0100

    3.2 added + Comments

--- SE3Logik/Abgabe 05/SE3LP_04_05_AS_JE.pl
+++ SE3Logik/Abgabe 05/SE3LP_04_05_AS_JE.pl
@@ -17,7 +17,7 @@ P kann nicht sowohl mit 1 als auch mit 2 unifiziert werden.
 g(f(s,R),f(R,s)) = g(f(S,s(T)),f(t(t)),S).
 false.
 g/2 laesst sich nicht mit g/3 unifizieren, da es 2 unterschiedliche
-Pr‰dikate darstellt.
+PrÔøΩdikate darstellt.
 
 true = not(not(True)).
 false.
@@ -47,7 +47,7 @@ lt(s(X),s(Y)):-lt(X,Y).
 %nachfolger(?Input,?Ergebnis)
 nachfolger(Input,Ergebnis):-
 	peano(Input),
-% Der Nachfolger ist der Vorg‰nger ummantelt mit dem Fakt s().
+% Der Nachfolger ist der VorgÔøΩnger ummantelt mit dem Fakt s().
 	Ergebnis = s(Input).
 
 %Test:
@@ -139,7 +139,7 @@ peano_to_int(s(Peano), Int) :-
 	peano(s(Peano)),
 % rekursiver Aufruf der Zahl
         peano_to_int(Peano, X),
-% F¸r jeden Aufruf eine Addition mit 1
+% FÔøΩr jeden Aufruf eine Addition mit 1
         Int is X + 1.
 
 
@@ -158,7 +158,7 @@ true.
 ?- ltTyped(s(s(0)),s(0)).
 false.
 */
-%Es wird false sowohl f¸r nein, als auch f¸r falsche Typen
+%Es wird false sowohl fÔøΩr nein, als auch fÔøΩr falsche Typen
 %ausgegeben.
 
 addTyped(0,X,X).
@@ -173,7 +173,7 @@ X = s(s(s(0))).
 77 ?- addTyped(s(s(1)),s(0),X).
 false.
 */
-% Wir stellen keine Ver‰nderung fest (false wenn falsche Eingabe),
+% Wir stellen keine VerÔøΩnderung fest (false wenn falsche Eingabe),
 % Ergebnis wenn richtige.
 
 
@@ -186,28 +186,52 @@ find_all_unterprodukte(KId, PId) :-
     kategorie(UKId, _, KId),
     find_all_unterprodukte(UKId, PId).
 
-verkaufspreis(KId,Jahr,Preis):-
+verkaufspreis(PId,KId,Jahr,Preis):-
+	%Finde alle Unterprodukte einer Kategorie
 	find_all_unterprodukte(KId, PId),
+	%Ermittel den jeweiligen Verkaufspreis
 	verkauft(PId,Jahr,Preis,_).
 
+%avg_verkaufspreis(?KId,Jahr,Avg_preis
 avg_verkaufspreis(KId,Jahr,Avg_preis):-
-	findall(Preis,verkaufspreis(KId,Jahr,Preis),Preisliste),
+	%Finde alle Preise einer Kategorie
+	findall(Preis,verkaufspreis(PId,KId,Jahr,Preis),Preisliste),
+	%Summiere die Preise auf
 	sum_list(Preisliste, Preis_summe),
+	%Anzahl der Preise
 	length(Preisliste, Laenge),
 	\+ Laenge == 0,
+	%Berechnung des Durchschnitts
 	Avg_preis is Preis_summe / Laenge.
 
 %Beispiel:
-%avg_verkaufspreis(0,2012,Avg).
+%?-avg_verkaufspreis(0,2012,Avg).
 %Avg = 19.
 
 %2.:
+%Hilfsfunktion zum Finden des Maximums einer Liste
+max([X],X).
+max([X|Xs],X):- max(Xs,Y), X >=Y.
+max([X|Xs],N):- max(Xs,N), N > X.
 
+%teuerstes_produkt(?PId,KId,Jahr)
+teuerstes_produkt(PId,KId,Jahr):-
+	%Finde alle Preise
+	findall(Preis,verkaufspreis(PId,KId,Jahr,Preis),Verkaufsliste),
+	%Finde den maximalen Preis
+	max(Verkaufsliste,MaxPreis),
+	%Suche nachen den Produkt mit den h√∂chsten Preis
+	verkaufspreis(PId,KId,Jahr,MaxPreis).
+	
+%Beispiel:
+%?-teuerstes_produkt(PId,KId,2010).
+%	
 
 
 %---------Aufgabe 4-----------
 
-%Beispiel Datenbank
+%Beispiel Datenbank f√ºr Voraussetzungen
+%A ist Voraussetzung f√ºr B
 ist_voraussetzung_fuer(anfang,beton_anmischen).
 ist_voraussetzung_fuer(anfang,grundriss).
 ist_voraussetzung_fuer(dachstuhl_errichten,dach_decken).
@@ -218,3 +242,6 @@ ist_voraussetzung_fuer(mauerbau,fenster_einsetzen).
 ist_voraussetzung_fuer(fenster_einsetzen,heizungs_installation).
 ist_voraussetzung_fuer(grundriss,mauerbau).
 ist_voraussetzung_fuer(beton_anmischen,fundament).
+
+ist_voraussetzung_fuer(A,B):-
+	ist_voraussetzung_fuer(C,A).
