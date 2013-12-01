51664638991c5ebbb39559f71841cceab2a5799e
Arnatar <arnestruck@astruck>
2013-12-01 16:51:18 +0100

    2&3, es fehlen comments

--- SE3Logik/Abgabe 06/SE3LP_04_06_AS_JE.pl
+++ SE3Logik/Abgabe 06/SE3LP_04_06_AS_JE.pl
@@ -12,7 +12,7 @@ zins_rek(Anlagebetrag,Zinsfaktor,Anlagedauer,Endguthaben):-
 	Anlagedauer == 0,
 	Endguthaben = Anlagebetrag;
 	\+ Anlagedauer == 0,
-	%Berechnung f√ºr ein Jahr
+	%Berechnung fuer ein Jahr
 	Guthaben is (1 + Zinsfaktor) * Anlagebetrag,
 	Restdauer is Anlagedauer - 1,
 	%Rekursiver Aufruf
@@ -24,7 +24,7 @@ zins_iter(Anlagebetrag,Zinsfaktor,Anlagedauer,Endguthaben):-
 	Zinsen is Zinssatz ** Anlagedauer,
 	%Abschluss der Zinsformel K * 1 + z^t
 	Endguthaben is Anlagebetrag * Zinsen.
-	
+
 %3.:
 % noch nicht fertig
 
@@ -41,7 +41,7 @@ zins_steigend(Anlagebetrag,Zinsfaktor,Bonuszins,Anlagedauer,Endguthaben):-
 	NeuerBonus is Bonuszins / 2,
 	%Rekursiver Aufruf
 	zins_steigend(Guthaben,NeuerZins,NeuerBonus,Restdauer,Endguthaben).
-	
+
 
 %5.:
 
@@ -49,6 +49,32 @@ zins_steigend(Anlagebetrag,Zinsfaktor,Bonuszins,Anlagedauer,Endguthaben):-
 %-----------Aufgabe 2------------
 
 %1.:
+% Nicht endrekursiv
+% Abbruchbedingung: (Wenn 4 ereicht ist, wird abgebrochen)
+pi(1, 4) :- !.
+
+pi(Steps, Result) :-
+% Steps mit jeder Rekursion um 1 reduzieren
+    NSteps is Steps - 1,
+    pi(NSteps, NResult),
+% Result ist das entsprechende Level der Reihe zu dem Rest addiert
+    Result is NResult + (4 * (-1 ** (Steps+1))/((2*Steps) - 1)).
+
+%Endrekursiv:
+% wrapper-praedikat
+endRekPi(Steps,Result):-
+	endRekPi(Steps,0,Result).
+
+% Abbruchbedingung
+endRekPi(0,PriorResult,PriorResult):-!.
+
+% Rekursion
+endRekPi(Steps,Prior,Result):-
+% NResult ist die
+	NResult is Prior + (4 * (-1 ** (Steps + 1))/((2 * Steps)-1)),
+% Steps um 1 reduzieren
+	NSteps is Steps -1,
+	endRekPi(NSteps,NResult,Result).
 
 %2.:
 
@@ -58,14 +84,71 @@ zins_steigend(Anlagebetrag,Zinsfaktor,Bonuszins,Anlagedauer,Endguthaben):-
 %-----------Aufgabe 3------------
 
 %1.:
+%nat_zahl(-Resultat)
+natN(Resultat):-
+    natZahlHelper(Resultat, 0).
+
+natNH(Result, Max):-
+    Result is Max + 1;
+    NewMax is Max + 1,
+    natNH(Result, NewMax).
+
+% ?- natZahl(R).
+% R = 1 ;
+% R = 2 ;
+% R = 3 ;
+% R = 4 ;
+% R = 5 ;
+% R = 6 ;
+% R = 7 ;
+% R = 8 ;
+% R = 9 ;
+% R = 10
+
 
 %2.:
+% nat_zahl(-Resultat, +Limit)
+natN2(Resultat, Limit):-
+    natN2H(Resultat, 0, Limit).
+natN2H(Resultat, Max, Limit):-
+    Resultat is Max + 1, Resultat =< Limit;
+    NewMax is Max + 1,
+    NewMax =< Limit,
+    natN2H(Resultat, NewMax, Limit).
+
+% ?- natZahl(R, 6).
+% R = 1 ;
+% R = 2 ;
+% R = 3 ;
+% R = 4 ;
+% R = 5 ;
+% R = 6 ;
+% false.
 
 %3.:
+pi_incr(Steps,Pi):-
+% Fuer alle Zahlen aus den Natuerlichen Zahlen wird eine Piberechnung
+% ausgef¸hrt mit den Zahlen als Schritte.
+	natN2(PiSteps,Steps),
+	endRekPi(PiSteps,Pi).
+/*
+?- pi_incr(4,Pi).
+Pi = 4 ;
+Pi = 2.666666666666667 ;
+Pi = 3.466666666666667 ;
+Pi = 2.8952380952380956 ;
+false.
+*/
 
 %4.:
+[display].
+%?- findall(X,pi_incr(119,X),L),display('pi',L).
+% Damit wird durch 119 der Anfang einer Linie erreicht, also sind nach
+% 119 Approximationen die Approximationsfehler unter die Genauigkeiten
+% der Darstellung gesunken.
 
 
 %-----------Aufgabe 4------------
 
 %1.:
+
