9de8df1729bc5c7b13146c96348711864c669205
Arnatar <arnestruck@astruck.de>
2014-10-16 21:03:53 +0200

    map nochmal

--- hlr/01/01-C/map.c
+++ hlr/01/01-C/map.c
@@ -1,47 +1,51 @@
 #include <stdio.h>
 
 // Definieren Sie ein enum cardd
-typedef enum {None, N, E, S, W} cardd;
-
-// Wir brauchen 'None', da bei der Array-Initialisierung der map per default alle
-// cells auf 0 initialisiert werden, und so per default 'N' gelten würde - das wollen
-// wir nicht. Eine alternative Implementation mit nur 4 Elementen waere:
-// typedef enum {N=1, E=2, S=3, W=4} cardd;
-
+typedef enum cardd {
+	N = 1,
+	E = 1 << 1,
+	S = 1 << 2,
+	W = 1 << 3,
+} cardd;
 
 // Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
-static cardd map[3][3];
-
+cardd map[3][3] = {};
 
 // Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
 // Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
 // Überprüfen Sie außerdem dir auf Gültigkeit
 void set_dir (int x, int y, cardd dir)
 {
-  if (x < 3 && x >= 0 && y < 3 && y >= 0) {
-    if (dir >= 0 && dir <= 4) {
-      map[x][y] = dir;
-    }
-  }
+	map[x][y] = dir;
 }
 
 // Die Funktion show_map soll das Array in Form einer 3x3-Matrix ausgeben
 void show_map (void)
 {
-  char* repr;
-  for (int x=0; x<3; x++) {
-    for (int y=0; y<3; y++) {
-      switch(map[x][y]) {
-        case N: repr = "N"; break;
-        case E: repr = "E"; break;
-        case S: repr = "S"; break;
-        case W: repr = "W"; break;
-        default: repr = "0";
-      }
-      printf(y<2 ? "%s   " : "%s\n", repr);
-    }
-  }
-
+	char* repr;
+	for (int x = 0; x < 3; x++) {
+		for (int y = 0; y < 3; y++) {
+			// hardcoding ist zwar eigentlich blöd, aber naja
+			// bug somewhere here:
+			switch (map[x][y]) {
+				case N|W: repr = "NW"; break;
+				case N  : repr = "N" ; break;
+				case N|E: repr = "NE"; break;
+				case E  : repr = "E" ; break;
+				case W  : repr = "W" ; break;
+				case S|E: repr = "SE"; break;
+				case S  : repr = "S" ; break;
+				case S|W: repr = "SW"; break;
+				default : repr = "0" ;
+			}
+			// same here
+			if (x == 1) {
+				printf(y < 2 ? "%s   " : "%s\n", repr);
+			} else {
+				printf(y < 2 ? "%s  " : "%s\n", repr);
+			}
+		}
+	}
 }
 
 int main (void)
