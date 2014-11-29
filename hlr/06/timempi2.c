0d09e0748b86c8dec8c657b203436d667f0a7f0c
Arnatar <arnestruck@astruck.de>
2014-11-29 14:52:58 +0100

    task 2

--- hlr/06/timempi2.c
+++ hlr/06/timempi2.c
@@ -1,21 +1,35 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h> 
+#include <time.h>
 #include <sys/time.h>
+#include <limits.h>
 
 const int msglength = 512;
-const int hnamelen = 64;
+const int hnamelen = 50;
 
 int size, rank;
 
 void master() {
+	// igno master in loop
 	for (int i = 1; i < size; i++) {
 		MPI_Status stat;
 		char msg_buf[msglength];
+		// receive from slaves
 		MPI_Recv(msg_buf, msglength, MPI_CHAR, i, 0, MPI_COMM_WORLD, &stat);
 		printf("%s\n", msg_buf);
 	}
+
+	//ignore this one
+	long current_msec = LONG_MAX;
+
+	//accumulator
+	long min_msec = LONG_MAX;
+
+	//master Reduce
+	MPI_Reduce(&current_msec, &min_msec, 1, MPI_LONG, MPI_MIN, 0, MPI_COMM_WORLD);
+
+	printf("Minimal Mikroseconds: %ld\n", min_msec);
 }
 
 void slave() {
@@ -31,10 +45,22 @@ void slave() {
 	long current_sec = (long) time.tv_sec;
 	long current_msec = (long) time.tv_usec;
 
-	// generate and send msg
+	// generate and formatter msg 
+	struct tm* mainTime;
+	mainTime = localtime(&time.tv_sec);
+
+	char formatedDate[msglength / 2];
 	char msg[msglength];
-	snprintf(msg, msglength, "%s: %ld.%ld", p_name, current_sec, current_msec);
-	MPI_Send(msg, strnlen(msg, msglength), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+
+	strftime(formatedDate, sizeof(formatedDate), "%Y-%m-%d %H:%M:%S", mainTime);
+
+	snprintf(msg, msglength, "%s: %s.%ld", p_name, formatedDate, current_msec);
+	
+	// send msg
+	MPI_Send(msg, msglength, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+
+	// reduce slaves, here no receive buffer
+	MPI_Reduce(&current_msec, NULL, 1, MPI_LONG, MPI_MIN, 0, MPI_COMM_WORLD);
 
 }
 
