c8046666024160f5891c34bacf711e2cd981b7b0
Arnatar <arnestruck@astruck.de>
2014-11-29 13:13:58 +0100

    structure change

--- hlr/06/timempi2.c
+++ hlr/06/timempi2.c
@@ -9,17 +9,16 @@
 
 int size, rank;
 
-
-int main(int argc, char** argv) {
-	// MPI_Init + Fehlermeldung
-	if(MPI_Init(&argc, &argv) != MPI_SUCCESS) {
-		printf("Error whith MPI_Init");
-		return -1;
+void master() {
+	for (int i = 1; i < size; i++) {
+		MPI_Status stat;
+		char msg_buf[MSGLENGTH];
+		MPI_Recv(msg_buf, MSGLENGTH, MPI_CHAR, i, 0, MPI_COMM_WORLD, &stat);
+		printf("%s\n", msg_buf);
 	}
+}
 
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+void slave() {
 
 	// get hostname
 	char p_name[HNAMELENGTH];
@@ -37,16 +36,24 @@ int main(int argc, char** argv) {
 	snprintf(msg, MSGLENGTH, "%s: %ld.%ld", p_name, current_sec, current_msec);
 	MPI_Send(msg, strnlen(msg, MSGLENGTH), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
+}
 
-	// gather msgs at rank 0
-	char* receive_buff;
-	if (rank == 0) {
-		for (int i = 1; i < size; i++) {
-			MPI_Status stat;
-			char msg_buf[MSGLENGTH];
-			MPI_Recv(msg_buf, MSGLENGTH, MPI_CHAR, i, 0, MPI_COMM_WORLD, &stat);
-			printf("%s\n", msg);
-		} 
+
+int main(int argc, char** argv) {
+	// MPI_Init + Fehlermeldung
+	if(MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+		printf("Error whith MPI_Init");
+		return -1;
+	}
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	// master-slave-diff
+	if(rank == 0) {
+		master();
+	} else {
+		slave();
 	}
 
 	// Beenden der Prozesse und entsprechende Ausgabe
