018fad7319497a22f632b6e65ee5443349eaa733
knut goetz <knutgoetz@outlook.com>
2014-12-12 18:26:47 +0100

    Kommentare

--- gwv/09/src/HMM_Model.java
+++ gwv/09/src/HMM_Model.java
@@ -11,18 +11,19 @@ import java.util.HashMap;
  * Tag, welches allein von dem i-1-tem Tag abhängt. 
  * Wir benötigen also die Übergangswahrscheinlichkeiten für die Tags P(Ti|Ti-1)
  * Außerdem benötigen wir die Emissionswahrscheinlichkeiten (Wi|Ti)
- *  
+ * Beides wird in dem PosTag-gespeichert.
  */
 
 public class HMM_Model {
 	final  File file;
+        // Mapping zwischen String im File und PosTag Objekt 
 	HashMap<String,PosTag> tags =  new HashMap<String, PosTag>();
-	
+   
 	public HMM_Model(String filepath) throws IOException{
 		this.file = new File(filepath);
 		
 	}
-	
+        // Aus den Trainingsdaten die Wahrscheinlichkeiten berechenen
 	public void analyize_file(){
 		 BufferedReader br;
 		try {
@@ -31,30 +32,29 @@ public class HMM_Model {
 			PosTag previous_tag = null;
 	        while((line = br.readLine()) != null) {
 	            String splitted[] = line.split("\\t");
-	            // Leerzeilen übersrpingen
+	            // ggf. Leerzeilen übersrpingen
 	            if (splitted.length >= 2){
 	            	// Splitte an TAB
 	            	String word  = splitted[0];
 	            	String tag =  splitted[1];
-	            	// System.out.println("Word: " + word + " Tag: " + tag ) ;
 	            	PosTag current_tag = tags.get(tag);
 	            	
 	            	if (current_tag == null){
 	            		// Tag zur Map hinzufügen, fall das erste mal aufgetaucht.
 	            		current_tag = new PosTag(tag);
 	            		tags.put(tag, current_tag);
-	            		//System.out.println("Neu hinzugefügt: " + current_tag.getTagname());
 	            	}
+			// Für die Berechnung der Emissionswahrscheinlichkeiten
 	            	current_tag.inc_count_words(word);
 	            	//  Spezialfall erste Zeile kein previous_tag beachten
 	            	if (previous_tag != null){
+			        // Für die Übergangswahrscheinlichkeiten 
 	            		previous_tag.inc_count_transition(current_tag);
-	            		//System.out.println("Transitions: " + transitions);
 	            	}
-	            	// count emission
 	            	previous_tag = current_tag;
 	            }
 	        }
+		//  Wahrscheinlichkeiten anhand der relativen Häufigkeit berechnen 
 	        normalize_probs();
 	        
 	    } 
@@ -62,7 +62,7 @@ public class HMM_Model {
 			e.printStackTrace();
 			}
 		}
-		
+        // Für alle Tags die Wahrscheinlichkeiten berechnen	
 	public void normalize_probs(){
 		for (PosTag tag : tags.values()){
 			tag.normalize_probabilities();
